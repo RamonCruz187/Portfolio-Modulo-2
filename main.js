cbfae947bf4d5090b0980cbf891ace5c1151059d
Ramon <ramon.cruz187@gmail.com>
2023-03-15 15:20:41 -0300

    se sube el maquetado de lModulo 2

---
+++ main.js
@@ -0,0 +1,16 @@
+let persona;
+
+function saludo(persona) {
+    if (persona=="hombre"){
+        console.log("Hola re contra capo");
+    }
+    else if (persona=="mujer"){
+        console.log("HOla señorita bonita");
+    }
+    
+    else{
+        console.log("Hola extraño");
+    }
+}
+
+saludo("nose");
