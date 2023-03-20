19806d467e95c39f62b4e67aa720d4fe7aa7eee0
Ramon <ramon.cruz187@gmail.com>
2023-03-19 22:10:08 -0300

    se agrega de apoco funcionalidad

--- main.js
+++ main.js
@@ -1,16 +1,76 @@
-let persona;
+/* function cPuesto(){
+    let divPuesto = document.getElementById("idPuesto"); 
 
-function saludo(persona) {
-    if (persona=="hombre"){
-        console.log("Hola re contra capo");
-    }
-    else if (persona=="mujer"){
-        console.log("HOla señorita bonita");
-    }
-    
-    else{
-        console.log("Hola extraño");
-    }
+    divPuesto.classList.remove("esconder");  
+    divPuesto.classList.add("prueba1");
+    let ocultar = document.getElementById("linkPuesto").classList.add("esconder"); 
+    let traerTexto = document.getElementById("textoPuesto").textContent;
+    let colocarTexto = document.getElementById("inputPuesto").value = traerTexto;
 }
 
-saludo("nose");
+function aPuesto(){ 
+    let nuevoTexto = document.querySelector("#inputPuesto"); 
+    let texto1 = document.querySelector("#textoPuesto"); 
+    texto1.textContent = nuevoTexto.value;  
+    let vuelta = document.getElementById("idPuesto");
+    vuelta.classList.add("esconder");
+    let ocultar = document.querySelector("#linkPuesto").classList.remove("esconder");
+
+}
+
+
+function cAcercaDe(){
+    let divPuesto = document.getElementById("idAcercaDe"); 
+
+    divPuesto.classList.remove("esconder");  
+    divPuesto.classList.add("prueba1");
+    let ocultar = document.getElementById("linkAcercaDe").classList.add("esconder"); 
+    let traerTexto = document.getElementById("textoAcercaDe").textContent;
+    let colocarTexto = document.getElementById("inputAcercaDe").value = traerTexto;
+}
+
+function aAcercaDe(){ 
+    let nuevoTexto = document.querySelector("#inputAcercaDe"); 
+    let texto1 = document.querySelector("#textoAcercaDe"); 
+    texto1.textContent = nuevoTexto.value;  
+    let vuelta = document.getElementById("idAcercaDe");
+    vuelta.classList.add("esconder");
+    let ocultar = document.querySelector("#linkAcercaDe").classList.remove("esconder");
+
+}
+ */
+
+class FuncionCambiar{
+    constructor(valor1,valor2){
+        this.valor1=valor1;
+        this.valor2=valor2;
+    }
+
+    cambiar(valor1,valor2){
+        this.textoACambiar = document.getElementById(valor1);
+        this.textoNuevo = document.getElementById(valor2);
+    }  
+}
+
+ function cambiarCasilla1(param){
+    const cambio1 = param.textoNuevo.value;
+    const cambio2 =param.textoACambiar;
+    cambio2.textContent=cambio1; 
+}
+
+function visualizar (param1){
+    const elementoVisualizar = param1;
+    elementoVisualizar.classList.remove("oculto");
+}
+
+function casa(param2){
+    const esconderVer = param2;
+    esconderVer.classList.add("oculto"); 
+}
+
+
+const editarProfesion = new FuncionCambiar;
+editarProfesion.cambiar("nombre1", "casilla1");
+
+const editar2 = new FuncionCambiar;
+editar2.cambiar("nombre2", "casilla2"); 
